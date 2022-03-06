d9b9fc9dbcdfd3620e72ece28bf0c722871a0c78
tluwaniMS <tluwani2020@gmail.com>
2022-03-06 09:07:46 +0200

    adding-logic-to-run-project-and-final-touches-to-main-graphql-schema

--- main-graphql-schema.js
+++ main-graphql-schema.js
@@ -1,13 +1,26 @@
 const { GraphQLObjectType, GraphQLSchema } = require("graphql");
+const {
+  deleteUserByIdMutation,
+  updateUserByIdMutation,
+  createUserMutation
+} = require("./graphql-mutations/UserMutation");
+const { getAllUsersQuery, getUserByIdQuery } = require("./graphql-queries/UserQuery");
 
 const Query = new GraphQLObjectType({
   name: "RootQuery",
-  fields: {}
+  fields: {
+    getAllUsers: getAllUsersQuery,
+    getUserById: getUserByIdQuery
+  }
 });
 
 const Mutation = new GraphQLObjectType({
   name: "Mutation",
-  fields: {}
+  fields: {
+    deleteUserById: deleteUserByIdMutation,
+    updateUserById: updateUserByIdMutation,
+    createUser: createUserMutation
+  }
 });
 
 module.exports = new GraphQLSchema({
