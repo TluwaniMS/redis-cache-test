09e01b7c619e09a0c4fe19a174679c7e52187d57
tluwaniMS <tluwani2020@gmail.com>
2022-03-06 12:53:30 +0200

    adding-cache-executors-to-query-resolvers

--- resolvers/user-query-resolvers.js
+++ resolvers/user-query-resolvers.js
@@ -11,29 +11,45 @@ const {
 } = require("../database-queries/user-database-queries");
 
 const getAllUsersQueryResolver = async () => {
-  const users = await getAllUsers();
+  let users;
 
   const cache = await getStoredDataInCache();
 
-  console.log(cache);
+  if (cache) {
+    console.log(`getting data from cache`);
+    users = JSON.parse(cache);
+  } else {
+    console.log(`getting data from database`);
+    users = await getAllUsers();
+    await storeUsersDataInCache(JSON.stringify(users));
+  }
 
   return users;
 };
 
 const deleteUserByIdMutationResolver = async (userId) => {
   await deleteUserById(userId);
+  const cache = await getStoredDataInCache();
+
+  cache ? deleteStoredDataInCache() : console.log(`No data has been cached`);
 
   return `Operation completed successfully.`;
 };
 
 const updateUserByIdMutationResolver = async (userId, userInfo) => {
   await updateUserById(userId, userInfo);
+  const cache = await getStoredDataInCache();
+
+  cache ? deleteStoredDataInCache() : console.log(`No data has been cached`);
 
   return `Operation completed successfully.`;
 };
 
 const createUserMutationResolver = async (userInfo) => {
   await createUser(userInfo);
+  const cache = await getStoredDataInCache();
+
+  cache ? deleteStoredDataInCache() : console.log(`No data has been cached`);
 
   return `Operation completed successfully.`;
 };
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const { app } = require("./app");
 const { getAllUsers } = require("./database-queries/user-database-queries");
 const { createSampleUsers } = require("./main-migration-script");
 const { connectToDataBase } = require("./database-config");
+const { client } = require("./redis-config");
 
 const PORT = process.env.PORT | 5000;
 
@@ -11,6 +12,7 @@ app.listen(PORT, () => {
 
 (async () => {
   await connectToDataBase();
+  await client.connect();
   const users = await getAllUsers();
 
   users.length === 0 ? await createSampleUsers() : console.log(`database has already been populated`);
